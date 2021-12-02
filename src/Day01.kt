cf1ee9c78161aa60187b502fdbfa0d5bcf02d2b6
Borja Quevedo <borjota@gmail.com>
2021-12-02 19:14:20 +0100

    ✨ Solution for Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,15 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }.windowed(2).count { (a, b) -> a < b }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }.windowed(4).count { it[0] < it[3] }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // val testInput = readInput("Day01_test")
+    // check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
